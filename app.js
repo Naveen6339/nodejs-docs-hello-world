0ae7c8928ae673ce2dfdccd307d3df74553ba5c5
Naveen6339 <drmsn6339@outlook.com>
2025-09-25 05:36:58 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+ const appMessage = process.env.AppMessage || "Default message";
+ res.send(appMessage);
+});
+app.listen(port, () => {
+ console.log(`App listening at http://localhost:${port}`);
+});
